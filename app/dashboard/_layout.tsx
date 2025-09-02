79fee8e342ad90ac09cbd80e1a2c33c2d51647cc
ishaniekanayaka <ishaniekanayaka27@gmail.com>
2025-09-02 13:21:47 +0530

    updated layout.tsx

--- app/dashboard/_layout.tsx
+++ app/dashboard/_layout.tsx
@@ -1,11 +1,52 @@
+// import { Tabs } from "expo-router";
+// import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+// import React from "react";
+
+// const tabs = [
+//   { label: "Home", name: "home", icon: "home-filled" },
+//   { label: "Profile", name: "profile", icon: "person" },
+//   { label: "Setting", name: "settings", icon: "settings" },
+// ];
+
+// const DashboardLayout = () => {
+//   return (
+//     <Tabs
+//       screenOptions={{
+//         tabBarActiveTintColor: "#007AFF",
+//         tabBarInactiveTintColor: "#999",
+//         headerShown: false,
+//         tabBarStyle: {
+//           backgroundColor: "#f8f8f8",
+//         },
+//       }}
+//     >
+//       {tabs.map(({ name, icon, label }) => (
+//         <Tabs.Screen
+//           key={name}
+//           name={name}
+//           options={{
+//             title: label,
+//             tabBarIcon: ({ color, size }) => (
+//               <MaterialIcons name={icon as any} color={color} size={size} />
+//             ),
+//           }}
+//         />
+//       ))}
+//     </Tabs>
+//   );
+// };
+
+// export default DashboardLayout;
 import { Tabs } from "expo-router";
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 import React from "react";
 
 const tabs = [
-  { label: "Home", name: "home", icon: "home-filled" },
+  { label: "Home", name: "index", icon: "home" },
+  { label: "Health", name: "health", icon: "medical-services" },
+  { label: "Diet", name: "diet", icon: "restaurant" },
+  { label: "Vets", name: "vets", icon: "local-hospital" },
   { label: "Profile", name: "profile", icon: "person" },
-  { label: "Setting", name: "settings", icon: "settings" },
 ];
 
 const DashboardLayout = () => {
@@ -17,6 +58,8 @@ const DashboardLayout = () => {
         headerShown: false,
         tabBarStyle: {
           backgroundColor: "#f8f8f8",
+          paddingBottom: 5,
+          height: 60,
         },
       }}
     >
